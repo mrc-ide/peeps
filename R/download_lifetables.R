4fce73fdaa9c0d7d80eeb1b6edc9b3dd45ea3b90
Ines <inesgerardursin@gmail.com>
2022-01-07 13:21:48 +0000

    Update download_lifetables.R

--- R/download_lifetables.R
+++ R/download_lifetables.R
@@ -19,19 +19,25 @@
 #' @examples
 #' download_undp_life_table(year=2.5, type="A", path="./data/")
 #' download_undp_life_table(2.5, "Complete", "C:/Users/ines/population/data/")
+#' lifetable_2.5_complete <-download_undp_life_table(2.5, "Complete", "C:/Users/ines/population/data/")
 download_undp_life_table <- function(year = 1, type = "complete", path) {
   
   # making sure that the type formatting is converted to the correct format
   type <- tolower(type)
   type <- ifelse(startsWith(type, "a"), "abridged", "complete")
+  
+  # Year=1 and type="Complete" stored at different URL pattern than the rest, so handle this with 
+  # if statement for now.
   if (year==1 & type=="complete"){
     url <- "https://www.un.org/en/development/desa/population/publications/pdf/mortality/EMLT/MLT_UN2011_130_1y_complete.xlsx"
   } else {
     url <- paste0("https://www.un.org/development/desa/pd/sites/www.un.org.development.desa.pd/files/unpd_2011_mlt_130_",
                  year, "y_", type, ".xlsx")
   }
-  destfile<-paste0(path, year, "y_", type, "_lifetable.xlsx")
+  
+  destfile<-paste0(path, year, "y_", type, "_lifetable.xlsx") # save as excel file
   download.file(url, destfile)
+  
   data<-readxl::read_excel(destfile)
   return(data)
 }
